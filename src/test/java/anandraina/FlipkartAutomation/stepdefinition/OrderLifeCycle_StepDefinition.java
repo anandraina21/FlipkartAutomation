424475eddb7f80b1c29b846f702b6577e72facc3
anandraina21 <anandraina95@gmail.com>
2020-03-10 01:52:28 +0530

    Added expicit wait

--- src/test/java/anandraina/FlipkartAutomation/stepdefinition/OrderLifeCycle_StepDefinition.java
+++ src/test/java/anandraina/FlipkartAutomation/stepdefinition/OrderLifeCycle_StepDefinition.java
@@ -3,14 +3,16 @@ package anandraina.FlipkartAutomation.stepdefinition;
 import java.io.File;
 import java.io.IOException;
 import java.util.Set;
-
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import anandraina.FlipkartAutomation.constants.Constants;
 import io.cucumber.java.en.Given;
@@ -19,6 +21,7 @@ import io.cucumber.java.en.When;
 
 public class OrderLifeCycle_StepDefinition {
 	WebDriver chromedriver;
+	WebDriverWait wait;
 
 	@Given("^User opens the website$")
 	public void User_opens_the_website() {
@@ -26,30 +29,38 @@ public class OrderLifeCycle_StepDefinition {
 		chromedriver = new ChromeDriver();
 		chromedriver.manage().window().maximize();
 		chromedriver.get(Constants.Resources.URL);
+		wait = new WebDriverWait(chromedriver, 20);
 	}
 
 	@When("^User logs in to their account$")
 	public void User_logs_in_to_their_account() throws InterruptedException {
-		chromedriver.findElement(By.cssSelector(Constants.ElementFactory.EMAIL_TEXTFIELD))
-				.sendKeys(Constants.Resources.EMAIL);
-		chromedriver.findElement(By.xpath(Constants.ElementFactory.PASSWORD_PASSWORDFIELD))
-				.sendKeys(Constants.Resources.PASSWORD);
-		chromedriver.findElement(By.cssSelector(Constants.ElementFactory.LOGIN_BUTTON)).click();
-		Thread.sleep(4000);
+		WebElement email = wait.until(ExpectedConditions
+				.visibilityOfElementLocated(By.cssSelector(Constants.ElementFactory.EMAIL_TEXTFIELD)));
+		email.sendKeys(Constants.Resources.EMAIL);
+		WebElement passwd = wait.until(ExpectedConditions
+				.visibilityOfElementLocated(By.xpath(Constants.ElementFactory.PASSWORD_PASSWORDFIELD)));
+		passwd.sendKeys(Constants.Resources.PASSWORD);
+		WebElement login = wait.until(
+				ExpectedConditions.visibilityOfElementLocated(By.cssSelector(Constants.ElementFactory.LOGIN_BUTTON)));
+		login.click();
+		Thread.sleep(2000);
 	}
 
 	@Then("^User searches for product$")
 	public void User_searches_for_product() throws InterruptedException {
-		chromedriver.findElement(By.xpath(Constants.ElementFactory.SEARCHBOX_TEXTFIELD))
-				.sendKeys(Constants.Resources.PRODUCT_NAME);
-		Thread.sleep(2000);
-		chromedriver.findElement(By.xpath(Constants.ElementFactory.PRODUCT_LISTITEM)).click();
+		WebElement searchbox = wait.until(
+				ExpectedConditions.visibilityOfElementLocated(By.xpath(Constants.ElementFactory.SEARCHBOX_TEXTFIELD)));
+		searchbox.sendKeys(Constants.Resources.PRODUCT_NAME);
+		WebElement productList = wait.until(
+				ExpectedConditions.visibilityOfElementLocated(By.xpath(Constants.ElementFactory.PRODUCT_LISTITEM)));
+		productList.click();
 	}
 
 	@Then("^Add an item to cart$")
 	public void Add_an_item_to_cart() throws InterruptedException {
-		Thread.sleep(3000);
-		chromedriver.findElement(By.cssSelector(Constants.ElementFactory.PRODUCTNAME_LINKTEXT)).click();
+		WebElement productName = wait.until(ExpectedConditions
+				.visibilityOfElementLocated(By.cssSelector(Constants.ElementFactory.PRODUCTNAME_LINKTEXT)));
+		productName.click();
 		Thread.sleep(1000);
 		Set<String> handles = chromedriver.getWindowHandles();
 		String currentHandle = chromedriver.getWindowHandle();
@@ -58,7 +69,9 @@ public class OrderLifeCycle_StepDefinition {
 				chromedriver.switchTo().window(handle);
 			}
 		}
-		chromedriver.findElement(By.cssSelector(Constants.ElementFactory.ADDTOCART_BUTTON)).click();
+		WebElement addToCart = wait.until(ExpectedConditions
+				.visibilityOfElementLocated(By.cssSelector(Constants.ElementFactory.ADDTOCART_BUTTON)));
+		addToCart.click();
 	}
 
 	@Then("^Take screenshot of product in cart$")
@@ -72,20 +85,23 @@ public class OrderLifeCycle_StepDefinition {
 
 	@Then("^Remove the item from cart$")
 	public void Remove_the_item_from_cart() throws InterruptedException {
-		Thread.sleep(2000);
-		chromedriver.findElement(By.xpath(Constants.ElementFactory.REMOVE_LINKTEXT)).click();
-		Thread.sleep(1000);
-		chromedriver.findElement(By.cssSelector(Constants.ElementFactory.REMOVE_BUTTON)).click();
-		Thread.sleep(1000);
+		WebElement removeLinkText = wait.until(
+				ExpectedConditions.visibilityOfElementLocated(By.xpath(Constants.ElementFactory.REMOVE_LINKTEXT)));
+		removeLinkText.click();
+		WebElement removeButton = wait.until(
+				ExpectedConditions.visibilityOfElementLocated(By.cssSelector(Constants.ElementFactory.REMOVE_BUTTON)));
+		removeButton.click();
 	}
 
 	@Then("^Logout from the account$")
 	public void Logout_from_the_account() throws InterruptedException {
+		WebElement profileDropdown = wait.until(ExpectedConditions
+				.visibilityOfElementLocated(By.cssSelector(Constants.ElementFactory.PROFILESECTION_DROPDOWN)));
 		Actions actions = new Actions(chromedriver);
-		actions.moveToElement(
-				chromedriver.findElement(By.cssSelector(Constants.ElementFactory.PROFILESECTION_DROPDOWN))).perform();
-		Thread.sleep(1000);
-		actions.click(chromedriver.findElement(By.xpath(Constants.ElementFactory.LOGOUT_LISTITEM))).perform();
+		actions.moveToElement(profileDropdown).perform();
+		WebElement logout = wait.until(
+				ExpectedConditions.visibilityOfElementLocated(By.xpath(Constants.ElementFactory.LOGOUT_LISTITEM)));
+		actions.click(logout).perform();
 	}
 
 	@Then("^Close the browser$")
